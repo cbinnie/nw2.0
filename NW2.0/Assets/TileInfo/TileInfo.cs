433aecf34e8f2467b2efd517f56ebaa595335390
SevernayaZemlya <jay.aguera@gmail.com>
2017-03-19 11:28:00 -0700

    Tile Text

---
+++ NW2.0/Assets/TileInfo/TileInfo.cs
@@ -0,0 +1,48 @@
+﻿// Tile 1 - Brainstorming Session? What brainstorming session? -5
+// Tile 2 - Everyone has a neat idea! You vote for your favorite! +2
+// Tile 3 - You take some time to really hone your project plan.  Pictures are drawn, models are made, highfives are exchanged. +5
+// Tile 4 - You perhaps spend **too** long planning.  Maybe the haiku about error codes was a bit much. -2 
+// Tile 5 - You step outside your comfort zone. +1
+// Tile 6 - You *really* step outside your comfort zone. -1
+// Tile 7 - "I guess I can email you what I coded.  The file's kinda big; do you have a thumbdrive?" -5
+// Tile 8 - "Hey, why don't we just use GitHub?" + 3
+// Tile 9 - You get drawn into a vicious debate about the merits of recursion versus iteration. -2
+// Tile 10 - SWAG! +1
+// Tile 11 - Download all of the new programs! -3
+// Tile 12 - Snacks! +3
+// Tile 13 - You are very brave and do some cool networking! +1
+// Tile 14 - Lunch Break! +5
+// Tile 15 - You attend a really cool workshop! +1
+// Tile 16 - "This workshop is exactly what we need!" +3
+// Tile 17 - "Does anyone have a charger? I'm almost out of " -2
+// Tile 18 - Your teammate brought an extra charger! + 1
+// Tile 19 - Dinner Break! +5
+// Tile 20 - You scale down your project to meet time constraints. +2
+// Tile 21 - You found a super helpful mentor! +5
+// Tile 22 - NullPointerException -1
+// Tile 23 - "Wait, *which* version are you using?" -3
+// Tile 24 - "I think my Android lego kit is missing some pieces. Gosh, I sure hope this doesn't end up a metaphor for my hack." -2
+// Tile 25 - You built the Android robot!  At least something is working! +1
+// Tile 26 - Cannot connect to server! Ahhhhhh! -4
+// Tile 27 - You are getting sleeeeeeepy. -4
+// Tile 28 - Time to top up on the caffeine. +2
+// Tile 29 - You drink so much caffeine that you vibrate through the floor.  Luckily a nice guard let's you back inside. -2
+// Tile 30 - Zzzzzzzzz +1
+// Tile 31 - Zzzzzzzzz +1
+// Tile 32 - Zzzzzzzzz? -1
+// Tile 33 - Zzzzzzzzz +1
+// Tile 34 - You oversleep! -1
+// Tile 35 - Groggy codng leeds to tyops -2
+// Tile 36 - Your teammate had an epiphany overnight!  +4
+// Tile 37 - Too hungry to wait for breakfast -- McDonald's run! -1
+// Tile 38 - Fooooood! +4
+// Tile 39 - ArrayIndexOutOfBoundsException -1
+// Tile 40 - Mentors keep asking you if you're done yet. -1
+// Tile 41 - Coding-coding-coding-slack-coding-coding-slack-coding-slack-slack-slack-slack... -1
+// Tile 42 - Merge Conflict! -2
+// Tile 43 - git reset --Hard -3
+// Tile 44 - Merge complete!  +3
+// Tile 45 - It seems to be working, but you're not sure why... -1
+// Tile 46 - You get a second wind for the final stretch! +3 
+// Tile 47 - Your component works perfectly!  The code flowed through you like you were a channel for Turing himself. +5
+// Tile 48 - You give a kickass demo!  Wear your sticker with pride. +5
