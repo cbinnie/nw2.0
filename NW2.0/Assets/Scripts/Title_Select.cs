7bfc50fbfa6074a5c29d35bf657be9f7eacaccb0
Nicholas Chan <nicholas.s.chan@gmail.com>
2017-03-19 12:46:05 -0700

    filtered for the array of used prefabs

--- NW2.0/Assets/Scripts/Title_Select.cs
+++ NW2.0/Assets/Scripts/Title_Select.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class Title_Select : MonoBehaviour
 {
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject player3;
+	public GameObject player4;
+	public GameObject player5;
+	private ArrayList list_players = new ArrayList(5);
+
     public int playerCount;
     public GameManager gm;
     public Dropdown myPlayerSelect;         //Refer to myPlayerSelect.value + 2 for the number of players
@@ -35,5 +42,14 @@ public class Title_Select : MonoBehaviour
     public void sendPlayerCount()
     {
         gm.setNumberPlayers(this.playerCount);
+        list_players.Add(player1);
+		list_players.Add(player2);
+		list_players.Add(player3);
+		list_players.Add(player4);
+		list_players.Add(player5);
+
+        for (int i = 0 ; i < this.playerCount; i++) {
+			gm.playerList.Add(list_players[i]);
+        }
     }
 }
