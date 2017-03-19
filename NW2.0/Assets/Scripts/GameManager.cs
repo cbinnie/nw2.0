0512630441531b7e9caf84f9f9ba134cbeb8cf8f
Chris Dileo <chris.dileo@gmail.com>
2017-03-19 11:29:21 -0700

    Added more logic to game maanger, some setup in  main scene

--- NW2.0/Assets/Scripts/GameManager.cs
+++ NW2.0/Assets/Scripts/GameManager.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-    private ArrayList playerList;
+    public ArrayList playerList;
     private ArrayList tiles;
     private GameObject currentPlayer;
     private GameObject board;
-    public GameObject UI;
+    public GameObject rollUI;
     private int turnCounter = 0;
     private int currentRoll = 0;
 
     // Use this for initialization
     void Start () {
         DontDestroyOnLoad(gameObject);//keep the game manager around for next screen
+        // for debug testing without a scene transition
+        //if (SceneManager.GetActiveScene().buildIndex == 1)
+        //{
+        //    setNumberPlayers(2);
+        //}
 	}
 	
 	// Update is called once per frame
@@ -27,7 +32,8 @@ public class GameManager : MonoBehaviour {
     public void getRoll()
     {
         currentRoll = Random.Range(1, 6);
-        UI.SendMessage("setRoll", currentRoll);
+        rollUI.SendMessage("setRoll", currentRoll);
+        movePlayer(currentPlayer, currentRoll);
     }
 
     public void setNumberPlayers(int num)
@@ -35,5 +41,28 @@ public class GameManager : MonoBehaviour {
         playerList = new ArrayList(num);
         Debug.Log("Players: " + num.ToString());
         SceneManager.LoadScene(1);
+        rollUI = GameObject.Find("RollText");
+        setupScene1();
+    }
+
+    public void setupScene1()
+    {
+        switch (playerList.Capacity)
+        {
+            case 5: 
+            case 4:
+            case 3:
+            case 2:
+                break;
+            default:
+                Debug.Log("wrong number of players...");
+                break;
+        }
+    }
+
+    private void movePlayer(GameObject player, int roll)
+    {
+        PlayerScript ps = player.GetComponent<PlayerScript>();
+        ps.moveForward(roll);
     }
 }
