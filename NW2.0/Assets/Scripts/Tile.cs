b8d60cceb4794f07f916e79f787d384624fe3969
SevernayaZemlya <jay.aguera@gmail.com>
2017-03-18 21:14:55 -0700

    Tile script

---
+++ NW2.0/Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public Tile nextTile;
+    public string textEffect;
+    public int scoreEffect;
+
+	// Use this for initialization
+	void Start () {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Tile getNextTile()
+    {
+        return nextTile;
+    }
+    public string getTextEffect()
+    {
+        return textEffect;
+    }
+    public int getScoreEffect()
+    {
+        return scoreEffect;
+    }
+
+    public Vector3 getPosition()
+    {
+        return this.transform.position;
+    }
+
+
+}
