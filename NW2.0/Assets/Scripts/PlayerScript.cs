4441d99250148f2027714bd2485af9762bb47220
cbinnie <cbinnie.sfu.ca>
2017-03-19 08:50:55 -0700

    Add player movement

--- NW2.0/Assets/Scripts/PlayerScript.cs
+++ NW2.0/Assets/Scripts/PlayerScript.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
+	public int score;//would like this to be an object that can be displayed at all times
+	public Tile currTile;
+	public float numSteps;
+	public float delaySpeed;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		Vector3 target = currTile.getPosition ();
+		while (transform.position != target)
+		transform.position = Vector3.MoveTowards(transform.position, target, 100);
+		moveForward (1);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
+	}
+
+	public IEnumerator delayedMove(Vector3 target)
+	{
+		float step = numSteps * Time.deltaTime;
+		while (transform.position != target)
+		{
+			transform.position = Vector3.MoveTowards (transform.position, target, step);
+			yield return new WaitForSeconds(delaySpeed);
+		}
+	}
+
+	public void moveForward(int numSpaces)
+	{
+		//move to the appropriate tile
+		while (numSpaces > 0 && currTile.getNextTile())
+		{
+			currTile = currTile.getNextTile ();
+			numSpaces--;
+		}
+
+		//move to the new tile
+		StartCoroutine(delayedMove(currTile.getPosition ()));
+
+		//show the text for the tile
+		currTile.getTextEffect();//need to be able to display this
+
+		//update the user's score
+		score += currTile.getScoreEffect();
 	}
 }
