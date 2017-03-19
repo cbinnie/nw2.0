9064e9fa64f35afbea9ff4df1f7dca1b38761b64
Chris Dileo <chris.dileo@gmail.com>
2017-03-18 20:49:46 -0700

    Rough out of game manager

---
+++ NW2.0/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private ArrayList playerList;
+    private ArrayList tiles;
+    private GameObject currentPlayer;
+    private GameObject board;
+    private int turnCounter = 0;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
